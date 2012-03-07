9d3de36d2e2cad97e4cb647b9ff6ea91796a0784
Eduardo Sanchez <Edos21@gmail.com>
2012-03-07 12:44:04 -0430

    subiendo proyecto

---
+++ footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>footer</title>
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	<body>
+		<footer>
+			<nav>
+				<ul>
+				<li><a href="index.php">Inicio</a></li>
+				<li> - </li>
+				<li><a href="/nosotros">Nosotros</a></li>
+				<li> - </li>
+				<li><a href="/servicios">Servicios</a></li>
+				<li> - </li>
+				<li><a href="/ubicanos">Ubicanos</a></li>
+				<li> - </li>
+				<li><a href="/contacto">Contactanos</a></li>
+				<li> | </li>
+				<li><a href="/aviso" class="legal">Aviso Legal</a></li>
+				<li> - </li>
+				<li><a href="/politicas" class="legal">Politicas de privacidad</a></li>
+				</ul>
+			</nav>
+			<div id="Copyright">
+				Copyright SERBAR 2012
+			</div>
+		</footer>
+	</body>
+</html>
