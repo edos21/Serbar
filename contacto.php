e217bb0e21f4e9aa810761c9fb6068120920395d
Eduardo Sanchez <Edos21@gmail.com>
2012-03-08 11:50:51 -0430

    formulario

---
+++ contacto.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="cstyle.css" type="text/css" />
+	</head>
+	<body>
+		<section>
+			<form method="post">
+				<label>Contactanos</label>
+				<input type="text" name="name" value="Nombre" class="campo"/>
+				<input type="text" name="mail" value="Correo" class="campo"/>
+				<input type="text" name="website" value="Sitio Web (opcional)" class="sitio"/>
+				<textarea name="comentarios" rows="4" cols="37">Comentarios...</textarea>
+				<input type="submit" value="Enviar" class="botonlogin"/>
+			</form>
+		</section>
+	</body>
+</html>
