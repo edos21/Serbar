20d856c04cee10e0b1dbfcef25840741ff1f06d2
Eduardo Sanchez <Edos21@gmail.com>
2012-03-15 12:17:55 -0430

    ubicanos

---
+++ ubicanos/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<h3>Lara</h3>
+		<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=10.068417,-69.31504&amp;num=1&amp;ie=UTF8&amp;t=m&amp;ll=10.067657,-69.313903&amp;spn=0.014789,0.018282&amp;z=15&amp;output=embed">
+		</iframe><br />
+		<p>Carrera 21 entre calles 23 y 24, Edif. Centro Drolara, Piso 1 Ofic. 2, Zona centro Barquisimeto</p>
+		<small><a href="http://maps.google.com/maps?q=10.068417,-69.31504&amp;num=1&amp;ie=UTF8&amp;t=m&amp;ll=10.067657,-69.313903&amp;spn=0.014789,0.018282&amp;z=15&amp;source=embed" style="color:#0000FF;text-align:left">Ver mapa m&aacute;s grande</a>
+		</small>
+	</body>
+</html>
